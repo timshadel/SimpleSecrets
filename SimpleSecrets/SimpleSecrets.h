979159056cd63d43c77f333f05303771d4aa761c
Tim Shadel <tim@shadelsoftware.com>
2013-05-11 20:10:31 -0600

    Initial Commit

---
+++ SimpleSecrets/SimpleSecrets.h
@@ -0,0 +1,13 @@
+//
+//  SimpleSecrets.h
+//  SimpleSecrets
+//
+//  Created by Tim Shadel on 5/11/13.
+//  Copyright (c) 2013 Tim Shadel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SimpleSecrets : NSObject
+
+@end
---
+++ SimpleSecrets/SimpleSecrets.m
@@ -0,0 +1,13 @@
+//
+//  SimpleSecrets.m
+//  SimpleSecrets
+//
+//  Created by Tim Shadel on 5/11/13.
+//  Copyright (c) 2013 Tim Shadel. All rights reserved.
+//
+
+#import "SimpleSecrets.h"
+
+@implementation SimpleSecrets
+
+@end
