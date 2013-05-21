5a22ecd4e61ed11abb596e45b0063e8b4bbfe826
Tim Shadel <tim@shadelsoftware.com>
2013-05-21 13:07:25 -0600

    Add empty spec for packet tests

---
+++ SimpleSecretsSpecs/PacketSpec.m
@@ -0,0 +1,18 @@
+//
+//  PacketSpec.m
+//  SimpleSecrets
+//
+//  Created by Tim Shadel on 5/21/13.
+//  Copyright (c) 2013 Tim Shadel. All rights reserved.
+//
+
+#import "Kiwi.h"
+#import "Packet.h"
+
+SPEC_BEGIN(PacketSpec)
+
+describe(@"a secret packet", ^{
+    
+});
+
+SPEC_END
